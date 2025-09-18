2aefd396954626f8249a9011d0d9d218fc7d0ddf
Yem Daro <daroyem99@gmail.com>
2025-09-18 08:01:10 +0700

    student classes

---
+++ main.py
@@ -0,0 +1,23 @@
+class Student():
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def display_info(self):
+        print(f"Hi! I am {self.first_name} {self.last_name}")
+
+class GraduateStudent(Student):
+    def __init__(self, first_name, last_name, major):
+
+        super().__init__(first_name, last_name)
+
+        self.major = 'CS'
+
+
+
+
+
+s = Student("John", "Cena")  # 
+
+s.display_info()
+
